59af5c8d74cd2751e37d32dc6bb43df02212503f
Arthur Eduardo Moura <arthur.rmoura@gmail.com>
2023-09-22 20:45:59 -0300

    Início da população do banco de daddos

--- src/test/java/com/api/core/appl/TransformadorDatas.java
+++ src/test/java/com/api/core/appl/TransformadorDatas.java
@@ -2,6 +2,8 @@ package com.api.core.appl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
@@ -14,29 +16,43 @@ class TransformadorDatas {
 
 	@Test
 	void test() {
-		
+
 		try {
-			DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-				    .parseCaseInsensitive()
-				    .appendPattern("EEE MMM dd uuuu HH:mm:ss zXX (zzzz)")
-				    .toFormatter(Locale.ENGLISH);
 			
 			ArrayList<String> listaDataHoraZona = new ArrayList<String>();
-			String aux = "Wed Dec 12 2018 00:04:03 GMT-0200 (Hora oficial do Brasil)";
-			listaDataHoraZona.add(aux);
+
+			String file = "src/test/java/com/api/core/appl/datas.txt";
+			BufferedReader reader = new BufferedReader(new FileReader(file));
 			
-			for(String dataHoraZona : listaDataHoraZona) {
-				ZonedDateTime zonedDateTime = ZonedDateTime.parse(dataHoraZona.replace("(Hora oficial do Brasil)", "(Brasilia Time)"), formatter);
+			String currentLine = "";
+			while(currentLine != null) {
+				currentLine = reader.readLine();
+				if(currentLine != null) {
+					listaDataHoraZona.add(currentLine);
+				}
+			}
+			reader.close();
+
+			DateTimeFormatter formatter = new DateTimeFormatterBuilder().parseCaseInsensitive()
+					.appendPattern("EEE MMM dd uuuu HH:mm:ss zXX (zzzz)").toFormatter(Locale.ENGLISH);
+
+	
+
+			for (String dataHoraZona : listaDataHoraZona) {
+				System.out.println(dataHoraZona);
+				ZonedDateTime zonedDateTime = ZonedDateTime
+						.parse(dataHoraZona.replace("(Hora oficial do Brasil)", "(Brasilia Time)"), formatter);
 				long timestampPosicao = zonedDateTime.toEpochSecond();
 				String timezonePosicao = zonedDateTime.getZone().getId();
-				
+
 				System.out.println(timestampPosicao);
 				System.out.println(timezonePosicao);
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
 			fail("Falha na transformação de datas");
 		}
-		
+
 		return;
 	}
 
