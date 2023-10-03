365852dbe1deb9463568a2c7c8cf2756d76c4b8e
Arthur Eduardo Moura <arthur.rmoura@gmail.com>
2023-10-03 17:26:25 -0300

    Ajuste no script de criação de Beanstalk

--- create.sh
+++ create.sh
@@ -3,7 +3,7 @@ eb create \
 	--instance_type t2.small \
 	--keyname aws-appl-poi \
 	--region sa-east-1 \
-	--elb-type application \
+	--single \
 	--vpc \
 	--vpc.id vpc-e7f45783 \
 	--vpc.ec2subnets subnet-31dadc68,subnet-36feab52 \
